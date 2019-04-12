59f8952c2cf6601fe668a3fb5787bdb55174d5e9
dpayton23 <darien.payton71@gmail.com>
2019-04-12 13:14:51 -0400

    created SmurfForm component

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class SmurfForm extends React.Component {
+    state = {
+        name: '',
+        age: '',
+        height: '',
+    }
+}
